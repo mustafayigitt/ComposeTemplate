9527b938c92f3f1f08ee68baebe90606b1b8be15
Mustafa Yiğit <mustafa.yigit@apsiyon.com>
2023-09-06 14:24:29 +0300

    update mockk to latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
     // Testing
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.5")
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("io.mockk:mockk:1.13.7")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
